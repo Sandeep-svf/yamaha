98603f40c428cba887339a4c1d4785ef9274b80f
Sandeep Maurya <svf.sandeep@gmail.com>
2022-09-05 16:40:15 +0530

    5 August 2022

--- app/src/main/java/com/webnmobapps/yamaha/profile/ProfileFragment.java
+++ app/src/main/java/com/webnmobapps/yamaha/profile/ProfileFragment.java
@@ -7,12 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import com.webnmobapps.yamaha.R;
+import com.webnmobapps.yamaha.adapter.ExpandableListViewAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
+    private ExpandableListView expandableListView;
 
+    private ExpandableListViewAdapter expandableListViewAdapter;
+
+    private List<String> listDataGroup;
+
+    private HashMap<String, List<String>> listDataChild;
 
 
     @Override
@@ -21,6 +34,130 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_profile, container, false);
 
+
+
+        // initializing the views
+        initViews(view);
+
+        // initializing the listeners
+        initListeners();
+
+        // initializing the objects
+        initObjects();
+
+        // preparing list data
+        initListData();
+
+
         return  view;
     }
+
+
+    /**
+     * method to initialize the views
+     */
+    private void initViews(View view) {
+
+        expandableListView = view.findViewById(R.id.expandableListView);
+
+    }
+
+    /**
+     * method to initialize the listeners
+     */
+    private void initListeners() {
+
+        // ExpandableListView on child click listener
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+               /* Toast.makeText(
+                        getActivity(),
+                        listDataGroup.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataGroup.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();*/
+                return false;
+            }
+        });
+
+        // ExpandableListView Group expanded listener
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getActivity(),
+                        listDataGroup.get(groupPosition) + " " + getString(R.string.text_collapsed),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // ExpandableListView Group collapsed listener
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getActivity(),
+                        listDataGroup.get(groupPosition) + " " + getString(R.string.text_collapsed),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    /**
+     * method to initialize the objects
+     */
+    private void initObjects() {
+
+        // initializing the list of groups
+        listDataGroup = new ArrayList<>();
+
+        // initializing the list of child
+        listDataChild = new HashMap<>();
+
+        // initializing the adapter object
+        expandableListViewAdapter = new ExpandableListViewAdapter(getActivity(), listDataGroup, listDataChild);
+
+        // setting list adapter
+        expandableListView.setAdapter(expandableListViewAdapter);
+
+    }
+
+    /*
+     * Preparing the list data
+     *
+     * Dummy Items
+     */
+    private void initListData() {
+
+
+        // Adding group data
+        listDataGroup.add(getString(R.string.text_alcohol));
+
+
+        // array of strings
+        String[] array;
+
+        // list of alcohol
+        List<String> alcoholList = new ArrayList<>();
+        array = getResources().getStringArray(R.array.string_array_alcohol);
+        for (String item : array) {
+            alcoholList.add(item);
+        }
+
+
+        // Adding child data
+        listDataChild.put(listDataGroup.get(0), alcoholList);
+
+
+        // notify the adapter
+        expandableListViewAdapter.notifyDataSetChanged();
+    }
+    
 }
